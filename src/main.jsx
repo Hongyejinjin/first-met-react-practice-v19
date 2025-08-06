ae87be47afa137c7f28f9a2099d947a2bb9263c3
Inje Lee <inje@soaple.io>
2025-08-06 13:24:03 +0900

    Chapter03 practice updated.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+import Library from './chapter_03/Library';
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <Library />
     </StrictMode>
 );
