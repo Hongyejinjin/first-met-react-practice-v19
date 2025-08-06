2986cd0024408aeac8a41d4532ce29d16c918736
Inje Lee <inje@soaple.io>
2025-08-06 16:41:49 +0900

    Chapter06 practice updated.

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import App from './App.jsx';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <StrictMode>
-        <CommentList />
-    </StrictMode>
+    // <StrictMode>
+        <NotificationList />
+    // </StrictMode>
 );
