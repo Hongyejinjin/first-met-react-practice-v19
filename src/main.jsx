8aac8b2d6b134966a517484d05eeca94bdcd4536
Inje Lee <inje@soaple.io>
2025-08-07 13:56:37 +0900

    Chapter09 practice updated.

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,11 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <ConfirmButton />
+        <LandingPage />
     </StrictMode>
 );
