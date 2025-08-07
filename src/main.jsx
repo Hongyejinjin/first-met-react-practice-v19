7d6a83bb61fa0802bab82faa51efdfbb0ef2afa4
Inje Lee <inje@soaple.io>
2025-08-07 13:29:15 +0900

    Chapter08 practice updated.

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,11 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    // <StrictMode>
-        <Accommodate />
-    // </StrictMode>
+    <StrictMode>
+        <ConfirmButton />
+    </StrictMode>
 );
